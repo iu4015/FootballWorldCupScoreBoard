302d72c795b4854d6778fc9650494405d78ee84d
Ihor Polishchuk <pi020883@gmail.com>
2020-12-28 01:05:26 +0200

    Applying SRP for FootballScoreBoard class

--- FootballWorldCupScoreBoardLibrary/FootballScoreBoard.cs
+++ FootballWorldCupScoreBoardLibrary/FootballScoreBoard.cs
@@ -15,15 +15,6 @@ namespace FootballWorldCupScoreBoard
 
         public void StartGame(string homeTeam, string awayTeam)
         {
-            if (string.IsNullOrEmpty(homeTeam))
-                throw new ArgumentNullException(nameof(homeTeam));
-
-            if (string.IsNullOrEmpty(awayTeam))
-                throw new ArgumentNullException(nameof(awayTeam));
-            
-            if (homeTeam.ToUpper() == awayTeam.ToUpper())
-                throw new ArgumentException($"The {nameof(homeTeam)} cannot be the same as the {nameof(awayTeam)}");
-
             if (games.Values.Any(g => g.HomeTeam.ToUpper() == homeTeam.ToUpper() || g.AwayTeam.ToUpper() == homeTeam.ToUpper()))
                 throw new ArgumentException("The homeTeam has already been playing");
 
--- FootballWorldCupScoreBoardLibrary/Game.cs
+++ FootballWorldCupScoreBoardLibrary/Game.cs
@@ -1,15 +1,26 @@
-﻿namespace FootballWorldCupScoreBoard
+﻿using System;
+
+namespace FootballWorldCupScoreBoard
 {
     internal class Game
     {
-        public Game(string homeTeam, string awayTeam)
-        {
-            HomeTeam = homeTeam;
-            AwayTeam = awayTeam;
-        }
-
         public string HomeTeam { get; }
         public string AwayTeam { get; }
         public string GameId => HomeTeam + "-" + AwayTeam;
+        
+        public Game(string homeTeam, string awayTeam)
+        {
+            if (string.IsNullOrEmpty(homeTeam))
+                throw new ArgumentNullException(nameof(homeTeam));
+
+            if (string.IsNullOrEmpty(awayTeam))
+                throw new ArgumentNullException(nameof(awayTeam));
+
+            if (homeTeam.ToUpper() == awayTeam.ToUpper())
+                throw new ArgumentException($"The {nameof(homeTeam)} cannot be the same as the {nameof(awayTeam)}");
+
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+        }
     }
 }
